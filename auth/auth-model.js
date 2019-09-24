02217caa7abef6971fb3b8513008728181a89d50
Robert Benavidez <murabitbenavidez@gmail.com>
2019-09-24 12:57:51 -0400

    auth router and auth middleware

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -8,21 +8,21 @@ module.exports = {
 };
 
 function find() {
-  return db('users').select('id', 'username');
+  return db('employees').select('id', 'username');
 }
 
 function findBy(filter) {
-  return db('users').where(filter);
+  return db('employees').where(filter);
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user);
+  const [id] = await db('employees').insert(user);
 
   return findById(id);
 }
 
 function findById(id) {
-  return db('users')
+  return db('employees')
     .where({ id })
     .first();
 }
